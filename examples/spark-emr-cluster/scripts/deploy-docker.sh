3d828a1d92792eee978ac18834398f6414210151
Joshua Carver <jcarver989@gmail.com>
2022-06-06 14:28:46 -0700

    feat: add deploy-docker.sh example script

--- examples/spark-emr-cluster/scripts/deploy-docker.sh
+++ examples/spark-emr-cluster/scripts/deploy-docker.sh
@@ -1,31 +1,12 @@
 #!/usr/bin/env bash
-export AWS_REGION="us-west-2"
 
+
+# This script provides an example for how to submit a Spark job that uses a Docker image.
+
+export AWS_REGION="us-west-2"
 CLUSTER=$1
-ENTRYPOINT="s3://toy-emr-cluster-test/python/main.py"
-DOCKER_IMAGE="575767027669.dkr.ecr.us-west-2.amazonaws.com/emr-ecr-repo:pyspark"
 
 echo "Deploying..."
-
-STEPS=$(cat <<-EOF 
-  {
-    "Type": "CUSTOM_JAR",
-    "Name": "PySpark",
-    "ActionOnFailure": "CONTINUE",
-    "Jar": "command-runner.jar",
-    "Args": [
-      "spark-submit",
-      "--deploy-mode cluster",
-      "--conf spark.executorEnv.YARN_CONTAINER_RUNTIME_TYPE=docker",
-      "--conf spark.executorEnv.YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$DOCKER_IMAGE",
-      "--conf spark.yarn.appMasterEnv.YARN_CONTAINER_RUNTIME_TYPE=docker",
-      "--conf spark.yarn.appMasterEnv.YARN_CONTAINER_RUNTIME_DOCKER_IMAGE=$DOCKER_IMAGE",
-      "$ENTRYPOINT"
-    ]
-  }
-EOF
-)
-
 
 aws emr add-steps \
 --cluster-id $CLUSTER \
